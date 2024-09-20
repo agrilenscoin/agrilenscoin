c8cb631f63feff2065e1a15ec0ad78fe25c50ea7
naveen <navinmid@gmail.com>
2024-09-21 00:19:52 +0530

    Increasing links height

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <div className="flex gap-4 items-center flex-col">
           <a
-            className="rounded-full border border-solid border-black/[.08] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black/[.08] transition-colors text-center flex items-center justify-center hover:bg-[#f2f2f2] hover:border-transparent text-sm sm:text-base h-14 sm:h-14 px-4 sm:px-5 sm:min-w-44"
             href="/assets/certificates/TNPCBCertificate2024.pdf"
             target="_blank"
             rel="noopener noreferrer"
@@ -23,7 +23,7 @@ export default function Home() {
             Tamil Nadu Pollution Control Board Certificate
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black/[.08] transition-colors text-center flex items-center justify-center hover:bg-[#f2f2f2] hover:border-transparent text-sm sm:text-base h-14 sm:h-14 px-4 sm:px-5 sm:min-w-44"
             href="/assets/certificates/CPCBCertificate2024.pdf"
             target="_blank"
             rel="noopener noreferrer"
