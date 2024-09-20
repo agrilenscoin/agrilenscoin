99b5223b4160a80446504cf30eaee5d82a82d95a
naveen <navinmid@gmail.com>
2024-09-20 23:40:40 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Agrilens Plates & Cups | Eco-Friendly Plates and Cups",
+  description: "Discover sustainable dining solutions with our innovative plates and cups crafted from agricultural byproducts. Our mission is to provide environmentally friendly alternatives that combine functionality with sustainability. Perfect for any occasion, our products help reduce waste while supporting a greener future. Join us in making a positive impact on the planet!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
